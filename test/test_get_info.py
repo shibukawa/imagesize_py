e2b2001c39bd9aeb2d4534f80f3598fcc11a4063
Yoshiki Shibukawa <yoshiki@shibu.jp>
2026-03-03 12:33:48 +0900

    Use unittest in get_info tests instead of pytest

--- test/test_get_info.py
+++ test/test_get_info.py
@@ -1,42 +1,40 @@
-import pytest
 import os
+import unittest
+
 import imagesize
 
 
 imagedir = os.path.join(os.path.dirname(__file__), "images")
 
 
-def test_get_info_defaults():
-    info = imagesize.get_info(os.path.join(imagedir, "test.png"))
-    assert info.width == 802
-    assert info.height == 670
-    assert info.xdpi == 72
-    assert info.ydpi == 72
-    assert info.colors == 16777216
+class GetInfoTest(unittest.TestCase):
+    def test_get_info_defaults(self):
+        info = imagesize.get_info(os.path.join(imagedir, "test.png"))
+        self.assertEqual(info.width, 802)
+        self.assertEqual(info.height, 670)
+        self.assertEqual(info.xdpi, 72)
+        self.assertEqual(info.ydpi, 72)
+        self.assertEqual(info.colors, 16777216)
 
+    def test_get_info_selective_fields(self):
+        info = imagesize.get_info(os.path.join(imagedir, "test.png"), size=True, dpi=False, colors=False)
+        self.assertEqual(info.width, 802)
+        self.assertEqual(info.height, 670)
+        self.assertEqual(info.xdpi, -1)
+        self.assertEqual(info.ydpi, -1)
+        self.assertEqual(info.colors, -1)
 
-def test_get_info_selective_fields():
-    info = imagesize.get_info(os.path.join(imagedir, "test.png"), size=True, dpi=False, colors=False)
-    assert info.width == 802
-    assert info.height == 670
-    assert info.xdpi == -1
-    assert info.ydpi == -1
-    assert info.colors == -1
+    def test_legacy_aliases(self):
+        size = imagesize.get(os.path.join(imagedir, "test.jpg"))
+        dpi = imagesize.getDPI(os.path.join(imagedir, "test.jpg"))
+        self.assertEqual(size, (802, 670))
+        self.assertEqual(dpi, (72, 72))
 
+    def test_get_info_invalid_input(self):
+        with self.assertRaises(FileNotFoundError):
+            imagesize.get_info(os.path.join(imagedir, "missing-file.png"))
 
-def test_legacy_aliases():
-    size = imagesize.get(os.path.join(imagedir, "test.jpg"))
-    dpi = imagesize.getDPI(os.path.join(imagedir, "test.jpg"))
-    assert size == (802, 670)
-    assert dpi == (72, 72)
-
-
-def test_get_info_invalid_input():
-    with pytest.raises(FileNotFoundError):
-        imagesize.get_info(os.path.join(imagedir, "missing-file.png"))
-
-
-def test_shortcuts_invalid_input():
-    missing = os.path.join(imagedir, "missing-file.png")
-    assert imagesize.get(missing) == (-1, -1)
-    assert imagesize.getDPI(missing) == (-1, -1)
+    def test_shortcuts_invalid_input(self):
+        missing = os.path.join(imagedir, "missing-file.png")
+        self.assertEqual(imagesize.get(missing), (-1, -1))
+        self.assertEqual(imagesize.getDPI(missing), (-1, -1))
